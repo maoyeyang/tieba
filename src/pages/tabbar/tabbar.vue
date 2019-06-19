660fd28be2bdfd4b0a7d196d85c190735b8d803a
maoyeyang <673842307@qq.com>
2019-06-19 19:21:19 +0800

    init project

---
+++ src/pages/tabbar/tabbar.vue
@@ -0,0 +1,84 @@
+<template>
+  <nav class="tabbar">
+    <router-link tag="span"
+                 class="tabbar-item"
+                 to="/home">
+      <img class="tabbar-item-icon"
+           src="./home.png" />
+      <span class="tabbar-item-label">首页</span>
+    </router-link>
+    <router-link tag="span"
+                 class="tabbar-item"
+                 to="/member">
+      <img class="tabbar-item-icon"
+           src="./login.png" />
+      <span class="tabbar-item-label">进吧</span>
+    </router-link>
+    <router-link tag="span"
+                 class="tabbar-item"
+                 to="/top">
+      <div class="tabbar-item-center">×</div>
+    </router-link>
+    <router-link tag="span"
+                 class="tabbar-item"
+                 to="/shopcar">
+      <img class="tabbar-item-icon"
+           src="./bell.png" />
+      <span class="tabbar-item-label">消息</span>
+    </router-link>
+    <router-link tag="span"
+                 class="tabbar-item"
+                 to="/search">
+      <img class="tabbar-item-icon"
+           src="./user.png" />
+      <span class="tabbar-item-label">我的</span>
+    </router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Tabbar',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.tabbar
+  position: fixed
+  bottom: 0
+  left: 0
+  width: 100%
+  height: 60px
+  display: flex
+  box-shadow: 0px 0px 3px #747474
+  .tabbar-item
+    flex: 1
+    text-align: center
+    .tabbar-item-icon
+      padding-top: 5px
+      margin: 0 auto
+      height: 30px
+      width: 30px
+      display: block
+    .tabbar-item-label
+      font-size: 12px
+    .tabbar-item-center
+      width: 50px
+      height: 50px
+      margin: 0 auto
+      background-color: blue
+      border-radius: 25%
+      background: linear-gradient(to right, #02B0F0, #007BEE)
+      box-shadow: 2px 2px 15px #95C9FF
+      transform: translateY(-15px) rotate(45deg)
+      transform-origin: 50% 50%
+      text-align: center
+      line-height: 50px
+      color: #ffffff
+      font-size: 35px
+</style>
